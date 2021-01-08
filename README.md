1637a8d597f664cd20d5d65a8ebc409393a843a6
makeshbabu <makeshbabus@gmail.com>
2021-01-08 11:40:45 +0530

    panoromic image custom upload

---
+++ README.md
@@ -0,0 +1,4 @@
+
+
+Link For Panoromaic images:
+https://github.com/15Dkatz/react-vr-panos/blob/master/images/starry-sky.jpg
--- index.vr.js
+++ index.vr.js
@@ -1,34 +1,15 @@
-import React from 'react';
-import {
-  AppRegistry,
-  asset,
-  Pano,
-  Text,
-  View,
-} from 'react-vr';
+import React, {Component} from 'react';
 
-export default class Basics extends React.Component {
-  render() {
-    return (
+import { AppRegistry, asset, Pano, View } from 'react-vr';
+
+export default class Basics extends Component {
+  render(){
+    return(
       <View>
-        <Pano source={asset('chess-world.jpg')}/>
-        <Text
-          style={{
-            backgroundColor: '#777879',
-            fontSize: 0.8,
-            fontWeight: '400',
-            layoutOrigin: [0.5, 0.5],
-            paddingLeft: 0.2,
-            paddingRight: 0.2,
-            textAlign: 'center',
-            textAlignVertical: 'center',
-            transform: [{translate: [0, 0, -3]}],
-          }}>
-          hello
-        </Text>
+        <Pano source={asset('starry-sky.jpg')}></Pano>
       </View>
-    );
-  }
-};
+    )
+  }  
+}
 
-AppRegistry.registerComponent('Basics', () => Basics);
+AppRegistry.registerComponent('Basics', ()=>Basics);
