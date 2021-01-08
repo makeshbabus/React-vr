6e780fa982b573e26f5ac6eb18e3ca7145897776
makeshbabu <makeshbabus@gmail.com>
2021-01-08 13:17:07 +0530

    props done

--- index.vr.js
+++ index.vr.js
@@ -1,12 +1,31 @@
 import React, {Component} from 'react';
 
-import { AppRegistry, asset, Pano, View } from 'react-vr';
+import { AppRegistry, asset, Pano, View, Text } from 'react-vr';
+
+class Task extends Component{
+    render(){
+        return(
+            <Text style={{textAlign:'center', fontSize:0.2}}>Todo: {this.props.text}</Text>
+        )
+    }
+}
+
 
 export default class Basics extends Component {
   render(){
     return(
       <View>
         <Pano source={asset('starry-sky.jpg')}></Pano>
+        <View
+        style={{
+            transform:[{translate: [0,0,-3]}],
+            layoutOrigin: [0.5, 0.5]
+        }}
+        >
+            <Task text="Get groceries"/>
+            <Task text="Finish HomeWork"/>
+            <Task text="Do Laundry"/>
+        </View>
       </View>
     )
   }  
